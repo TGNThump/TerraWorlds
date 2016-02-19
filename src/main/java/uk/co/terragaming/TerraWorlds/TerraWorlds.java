cbff3df6326f050aa72a2280b984a0c828a953a3
Ben Pilgrim <ben@pilgrim.me.uk>
2016-02-19 13:22:08 +0000

    Initial Commit

---
+++ src/main/java/uk/co/terragaming/TerraWorlds/TerraWorlds.java
@@ -0,0 +1,9 @@
+package uk.co.terragaming.TerraWorlds;
+
+import uk.co.terragaming.TerraCore.Foundation.GuiceModule;
+import uk.co.terragaming.TerraCore.Foundation.Module;
+
+@Module(name = PomData.NAME, version = PomData.VERSION)
+public class TerraWorlds extends GuiceModule{
+	
+}
